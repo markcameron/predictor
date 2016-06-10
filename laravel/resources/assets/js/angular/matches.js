cf14b918291e72a35b72199afdfdea6f51d0d730
Mark Cameron <mark.cameron@devfactory.ch>
2016-06-10 19:48:51 +0200

    console.log prediction check

--- laravel/resources/assets/js/angular/matches.js
+++ laravel/resources/assets/js/angular/matches.js
@@ -44,7 +44,7 @@ angular.module('predictor', ['ngAnimate', 'ngTouch', 'ngDialog', 'vTabs'], ["$in
         $scope.openPrediction = function (match) {
 	    $scope.match = match;
 	    console.log(user.can_predict);
-	    if (user.can_predict) {
+	    if (user.can_predict != 0) {
 		ngDialog.open({
                     template: 'dialogPrediction',
                     controller: 'dialogPredictionCtrl',
