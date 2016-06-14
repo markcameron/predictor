fe7f2036256882375db4c184602b316cad4464da
Mark Cameron <mark.cameron@devfactory.ch>
2016-06-14 09:50:24 +0200

    Force remember token so users don't have to log in all the time

--- laravel/app/Http/Controllers/Auth/AuthController.php
+++ laravel/app/Http/Controllers/Auth/AuthController.php
@@ -9,6 +9,7 @@ use Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
@@ -50,6 +51,43 @@ class AuthController extends Controller
   public function redirectToProvider()
   {
     return Socialite::driver('facebook')->redirect();
+  }
+
+  /**
+   * Handle a login request to the application.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function login(Request $request)
+  {
+    $this->validateLogin($request);
+
+    // If the class is using the ThrottlesLogins trait, we can automatically throttle
+    // the login attempts for this application. We'll key this by the username and
+    // the IP address of the client making these requests into this application.
+    $throttles = $this->isUsingThrottlesLoginsTrait();
+
+    if ($throttles && $lockedOut = $this->hasTooManyLoginAttempts($request)) {
+      $this->fireLockoutEvent($request);
+
+      return $this->sendLockoutResponse($request);
+    }
+
+    $credentials = $this->getCredentials($request);
+
+    if (Auth::guard($this->getGuard())->attempt($credentials, TRUE)) {
+      return $this->handleUserWasAuthenticated($request, $throttles);
+    }
+
+    // If the login attempt was unsuccessful we will increment the number of attempts
+    // to login and redirect the user back to the login form. Of course, when this
+    // user surpasses their maximum number of attempts they will get locked out.
+    if ($throttles && ! $lockedOut) {
+      $this->incrementLoginAttempts($request);
+    }
+
+    return $this->sendFailedLoginResponse($request);
   }
 
   /**
