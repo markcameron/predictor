7c26d62288f245a2deeac0f466cde5dfd4931c76
Mark Cameron <mark.cameron@devfactory.ch>
2020-04-13 22:00:49 +0200

    Fix Assets in production

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -12,7 +12,9 @@ class AppServiceProvider extends ServiceProvider {
      * @return void
      */
     public function boot() {
-        //
+        if (config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 
     /**
